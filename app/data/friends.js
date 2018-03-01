6995010b96e382a9d258da72ab8866c52883e4c8
zrowe <paul.louis.rowe@gmailcom>
2018-02-28 22:28:34 -0800

    adding match and modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,90 @@
-var friends = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}];
+var friends = [
+
+    {
+        "name": "Adam Busby",
+        "photo": "https://i2.wp.com/www.illumni.co/wp-content/uploads/2014/06/No.-1.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ],
+    },
+
+    {
+        "name": "Spencer Versteeg",
+        "photo": "https://i0.wp.com/ignitemodelsblog.com/wp-content/uploads/2016/08/IMG_2828-2-2.jpg",
+        "scores": [
+            3,
+            5,
+            2,
+            1,
+            4,
+            2,
+            5,
+            5,
+            3,
+            2
+        ],
+    },
+
+
+    {
+        "name": "Ofelia Esquivel",
+        "photo": "https://hilton-production.s3.amazonaws.com/image/51/profile_thumbnail.jpg",
+        "scores": [
+            3,
+            2,
+            3,
+            1,
+            5,
+            1,
+            3,
+            1,
+            2,
+            4
+        ],
+    },
+
+    {
+        "name": "Ted Cruz",
+        "photo": "http://listverse.wpengine.netdna-cdn.com/wp-content/uploads/2017/07/10-ted-cruz.jpg",
+        "scores": [
+            2,
+            5,
+            5,
+            5,
+            2,
+            1,
+            2,
+            3,
+            1,
+            5
+        ],
+    },
+
+    {
+        "name": "Mortimer Snerd",
+        "photo": "http://media.liveauctiongroup.net/i/12926/13096800_2.jpg",
+        "scores": [
+            5,
+            5,
+            1,
+            1,
+            2,
+            3,
+            4,
+            1,
+            5,
+            5
+        ],
+    },
+];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,9 +11,29 @@ module.exports = function(app) {
 
     // A POST routes /api/friends. This will be used to handle incoming survey
     // results. This route will also be used to handle the compatibility logic.
-    // TODO: process the POST data
     app.post("/api/friends", function(req, res) {
-        var newFriend = req.body;
-        console.log(newFriend);
+        var newFriend = {};
+        newFriend.name = req.body.name;
+        newFriend.photo = req.body.photo;
+        newFriend.scores = req.body["scores[]"]; // WTF??
+        var match = findMatch(newFriend);
+        friends.push(newFriend);
+        res.send(match);
     })
+}
+
+function findMatch(newFriend) {
+
+    var best = { score: 999, index: null }; // lower is better
+    for (var i = 0; i < friends.length; i++) {
+        var totalDifference = 0;
+        for (var j = 0; j < friends[i].scores.length; j++) {
+            totalDifference = totalDifference + Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+        }
+        if (totalDifference < best.score) {
+            best.score = totalDifference;
+            best.index = i;
+        };
+    };
+    return friends[best.index]
 }
